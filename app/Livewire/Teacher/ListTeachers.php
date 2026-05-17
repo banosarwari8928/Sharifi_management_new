7c3e2024573afd785c612e7c180883a3964faca1
banosarwari8928 <banosarwari8928@gmail.com>
2026-05-17 14:44:12 +0430

    worked on list relationships between classes and students

--- app/Livewire/Teacher/ListTeachers.php
+++ app/Livewire/Teacher/ListTeachers.php
@@ -32,7 +32,7 @@ class ListTeachers extends Component implements HasActions, HasSchemas, HasTable
                 TextColumn::make('user.name')->label('name')->searchable()->sortable(),
                 TextColumn::make('user.email')->label('Email')->icon(Heroicon::Envelope)->iconColor('primary'),
                 TextColumn::make('last_name'),
-                TextColumn::make('sinf.title')->limitList(3)->badge(),
+                TextColumn::make('sinf.title')->limitList(3)->badge()->separator(","),
                 TextColumn::make('degree_of_education')->color('primary'),
                 TextColumn::make('phone_number'),
                 TextColumn::make('bio')->limit(20),
